ba16f6e5f1c19297ab27afdb92e4bef1706c6ecb
siris <nicolau.duransilva@sirisacademic.com>
2025-01-17 11:53:47 +0100

    fix: move setup.py to the root

--- setup.py
+++ setup.py
@@ -9,12 +9,13 @@ except FileNotFoundError:
 
 setup(
     name="citation-parser",
-    version="0.0.1",
+    version="1.0.0",
     author="Nicolau Duran-Silva, Pablo Accuosto, Ruggero Cortini",
     author_email="nicolau.duransilva@sirisacademic.com, pablo.accuosto@sirisacademic.com, ruggero.cortini@sirisacademic.com",
     description="CitationParser provides tools for processing raw citation and linking to objects in scholarly knowledge graphs.",
     url="https://github.com/sirisacademic/citation-parser",
-    packages=find_packages(),  # Automatically find packages within affilgood/
+    packages=find_packages(),  # Automatically find packages within citation_parser/
+    package_dir={"": "citation-parser"},  # Ensure the module path is correct
     install_requires=requirements,  # Load dependencies from requirements.txt
     python_requires=">=3.9",
     include_package_data=True,  # Include additional files specified in MANIFEST.in
